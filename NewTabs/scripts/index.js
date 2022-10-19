04282f67ab97a86382d5b0351e9749c4cba23d6a
Randy Rivera <rivera.na2015@gmail.com>
2022-10-19 09:33:07 -0400

    Opening and closing browser windows

---
+++ NewTabs/scripts/index.js
@@ -0,0 +1,13 @@
+function openFirstURL() {
+    window.open("https://www.google.com/")  
+}
+function openSecondURL() {
+    window.open("https://www.w3schools.com/js/")
+}
+function openThirdURL() {
+    window.open("images/1960x0.jpg")
+}
+
+function openImages() {
+    window.open("images.html", "_self") // using _self to open images 
+}
