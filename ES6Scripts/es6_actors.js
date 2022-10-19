a36f2a2ae7cc837b634f2a4e10ca45efc1ca5b5d
Randy Rivera <rivera.na2015@gmail.com>
2022-10-19 10:06:08 -0400

    made a few adjustments

--- ES6Scripts/es6_actors.js
+++ ES6Scripts/es6_actors.js
@@ -24,18 +24,35 @@ let academyMembers = [
 ];
 
 //member whose id is 187
+
+console.log(academyMembers.find((array) => array.memID == "187").name);
+
+// or
+
 let findAcademyMember = academyMembers.find((array) => array.memID == "187");
+
 if (findAcademyMember != undefined) {
     console.log("Member with a memID of 187 is " + findAcademyMember.name)
 }
 
 // whos been in at least 3 films
 let overthree = academyMembers.filter(n => n.films.length >= 3);
+
+overthree.forEach(n => console.log(">3 films: " + n.name))
+
+// or
+
 for (let i = 0; i < overthree.length; i++) {
     console.log("Who's been in at least three films? " + overthree[i].name)
 }
+
 // who has a name that starts with "Bob"
 let nameThatStarts = academyMembers.filter((array) => array.name.indexOf("Bob") != -1)
+
+nameThatStarts.forEach(n => console.log("Name that starts with Bob: " + n.name))
+
+// or
+
 for (let i = 0; i < nameThatStarts.length; i++) {
     console.log("Name that starts with Bob: " + nameThatStarts[i].name)
 }
@@ -51,4 +68,4 @@ for (let i = 0; i < findFilm.length; i++) {
             console.log("Academy Members that have been in a film that starts with A: " + findFilm[i].name)
         }
     }
-}
+} 
